511f5de39172585c5eb0e175c85abd36d162674e
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-06-30 14:16:56 +0900

    透視投影の完成

--- main.cpp
+++ main.cpp
@@ -259,10 +259,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	};
 
 	Vertex vertices[] = {
-			{ {  0.0f , 100.0f , 0.0f} , {0.0f,1.0f}},//左下 インデックス0
-			{ {  0.0f ,   0.0f , 0.0f} , {0.0f,0.0f}},//左上 インデックス1
-			{ {100.0f , 100.0f , 0.0f} , {1.0f,1.0f}},//右下 インデックス2
-			{ {100.0f ,   0.0f , 0.0f} , {1.0f,0.0f}},//右上 インデックス3
+			{ {-50.0f , -50.0f , 150.0f} , {0.0f,1.0f}},//左下 インデックス0
+			{ {-50.0f ,  50.0f , 150.0f} , {0.0f,0.0f}},//左上 インデックス1
+			{ { 50.0f , -50.0f , 150.0f} , {1.0f,1.0f}},//右下 インデックス2
+			{ { 50.0f ,  50.0f , 150.0f} , {1.0f,0.0f}},//右上 インデックス3
 	};
 
 	//インデックスデータ
@@ -399,11 +399,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	//単位行列を代入
 	constMapTransform->mat = XMMatrixIdentity();
-	//
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / window_width;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / window_height;
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	////
+	//constMapTransform->mat.r[0].m128_f32[0] = 2.0f / window_width;
+	//constMapTransform->mat.r[1].m128_f32[1] = -2.0f / window_height;
+	//constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
+	//constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	
+	////平行投影行列の計算	
+	//constMapTransform->mat = XMMatrixOrthographicOffCenterLH(
+	//	0.0f,100.0f,//左端、右端
+	//	100.0f,0.0f,//下端、上端
+	//	0.0f,1.0f//前端、奥端
+	//	);//LH = LeftHandの略
+
+	//透視投影行列の計算	
+	constMapTransform->mat = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),//上下画角45度
+		(float)window_width / window_height,//アスペクト比
+		0.1f, 1000.0f//前端、奥端
+		);
+	
 	//インデックスデータ全体のサイズ
 	UINT sizeIB = static_cast<UINT>(sizeof(uint16_t)) * _countof(indices);
 
