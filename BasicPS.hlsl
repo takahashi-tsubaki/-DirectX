96652a494df0893d86634d67c7c88bf5a3ead30d
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-07-22 14:03:03 +0900

    画像を表示させた

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -3,7 +3,15 @@
 Texture2D<float4> tex : register(t0);	//0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);		//0番スロットに設定されたサンプラー
 
-float4 main(VSOutput input) : SV_TARGET
-{
+float4 main(VSOutput input) : SV_TARGET{
+
+
+	////環境光
+	float3 light = normalize(float3(1, -1, 1));//右下奥　向きのライト
+	float diffuse = saturate(dot(-light, input.normal));//diffuseを[0,1]の範囲にclampする
+	float brightness = diffuse + 0.3f;//アンビエント項を0.3として計算
+
+	float4 texColor = float4(tex.Sample(smp, input.uv));
+	return float4(texColor.rgb * brightness, texColor.a) * color;
 	return float4(color);//RGBをそれぞれ法線のxyz、Aを1で出力
 }
