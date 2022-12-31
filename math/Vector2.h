908574372e8e50b92b60306f6acabd55e2ade889
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-12-31 21:44:43 +0900

    処理だけ書いたから実行はまだできていない

--- math/Vector2.h
+++ math/Vector2.h
@@ -1,41 +1,25 @@
 #pragma once
 
-class Vector2
-{
+/// <summary>
+/// 2次元ベクトル
+/// </summary>
+class Vector2 {
 public:
-	//コンストラクタ
-	Vector2();//零ベクトルとして生成
-	Vector2(float x, float y);//x成分,y成分を指定しての生成
+	float x; // x成分
+	float y; // y成分
 
-	//メンバ関数
-	float length() const;//ノルム（長さ）を求める
-	Vector2& normalize();//正規化をする
-	float dot(const Vector2& v) const;//内積を求める
-	float cross(const Vector2& v) const;//外積を求める
-
-	//単項演算子オーバーロード
-	Vector2 operator+ () const;//thisがかわらない
-	Vector2 operator- () const;
-
-	//代入演算子オーバーロード
-	Vector2& operator+= (const Vector2& v);
-	Vector2& operator-= (const Vector2& v);
-	Vector2& operator*= (float s);
-	Vector2& operator/= (float s);
-
-
-
-	//メンバ変数
 public:
-	float x;//x成分
-	float y;//y成分
-	//(x,y)
+	// コンストラクタ
+	Vector2();                          // 零ベクトルとする
+	Vector2(float x, float y); // x成分, y成分 を指定しての生成
+
+	// 単項演算子オーバーロード
+	Vector2 operator+() const;
+	Vector2 operator-() const;
+
+	// 代入演算子オーバーロード
+	Vector2& operator+=(const Vector2& v);
+	Vector2& operator-=(const Vector2& v);
+	Vector2& operator*=(float s);
+	Vector2& operator/=(float s);
 };
-
-//2項演算子オーバーロード
-//いろんな引数(引数の型と順序)のパターンに対応するため、以下のように準備している
-const Vector2 operator+(const Vector2& v1, const Vector2& v2);
-const Vector2 operator-(const Vector2& v1, const Vector2& v2);
-const Vector2 operator*(const Vector2& v, float s);
-const Vector2 operator*(float s, const Vector2& v);
-const Vector2 operator/(const Vector2& v, float s);
